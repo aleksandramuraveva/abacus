0661c30cb88c28e2c3f1d9b2b8068ea38e322f6d
aleksandramuraveva <84822235+aleksandramuraveva@users.noreply.github.com>
2024-11-13 01:23:48 +0300

    chore: delete unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,6 @@ import rodImage from './assets/Frame1.png'
 
 import './App.css';
 
-const Bead = ({ position, onMove, beadUrl }) => {
-  const handleDrag = (e) => {
-    if (e.clientY === 0) return; 
-
-    onMove(e);
-  };
-
-  return (
-    <div
-      className="bead-container"
-      style={{ top: `${position}px`, position: 'absolute' }}
-      draggable
-      onDrag={handleDrag}
-      onDragEnd={handleDrag}
-    >
-      <img src={beadUrl} alt="Bead" className="bead" />
-    </div>
-  );
-};
-
-
-
-
-
 const App = () => {
   const beadUrl = beadImage;
   const rodUrl = rodImage;
