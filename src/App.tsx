e2aa02ccf07a698e17b2ad0ec5366a5228a95c4f
aleksandramuraveva <84822235+aleksandramuraveva@users.noreply.github.com>
2024-11-13 04:43:23 +0300

    feat: implement dynamic rods

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+import { useState } from 'react';
 import { AbacusProvider } from './contexts/AbacusContext';
 import Header from './components/Header/Header';
 import Abacus from './components/Abacus/Abacus';
 import './App.css';
 
 const App = () => {
+  const [rodCount, setRodCount] = useState(3);
+
   return (
     <AbacusProvider>
       <div className="App">
-        <Header />
-        <Abacus />
+        <Header rodCount={rodCount} setRodCount={setRodCount} />
+        <Abacus rodCount={rodCount} />
       </div>
     </AbacusProvider>
   );
