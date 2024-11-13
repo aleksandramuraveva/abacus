afa0d0bfd61e676f1b9484463cdabb85af0b375b
aleksandramuraveva <84822235+aleksandramuraveva@users.noreply.github.com>
2024-11-13 21:13:27 +0300

    refactor: type contexts

--- src/contexts/AbacusContext.tsx
+++ src/contexts/AbacusContext.tsx
@@ -4,11 +4,40 @@ import bead2 from '../assets/Bead2.png';
 import frame1 from '../assets/Frame1.png';
 import frame2 from '../assets/Frame2.png';
 
-export const AbacusContext = createContext();
+interface Theme {
+  beadUrl: string;
+  rodUrl: string;
+  backgroundColor: string;
+  borderColor: string;
+}
 
-export const AbacusProvider = ({ children }) => {
+interface Themes {
+  [key: string]: Theme;
+}
+
+interface AbacusContextType extends Theme {
+  changeTheme: () => void;
+}
+
+const defaultContext: AbacusContextType = {
+  beadUrl: '',
+  rodUrl: '',
+  backgroundColor: '',
+  borderColor: '',
+  changeTheme: () => {}
+};
+
+
+interface AbacusProviderProps {
+  children: React.ReactNode;
+}
+
+
+export const AbacusContext = createContext<AbacusContextType>(defaultContext);
+
+export const AbacusProvider = ({ children } : AbacusProviderProps) => {
   const [theme, setTheme] = useState('theme1');
-  const themes = {
+  const themes: Themes = {
     theme1: { beadUrl: bead2, rodUrl: frame2, backgroundColor: 'var(--pink)', borderColor: 'var(--primary)' },
     theme2: { beadUrl: bead1, rodUrl: frame1, backgroundColor: 'var(--secondary)', borderColor: 'var(--brown)' },
   };
